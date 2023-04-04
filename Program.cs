340d6773b1a17bca7a0409ce9fb08b5d9a952013
ZestySalad <121341328+ZestySalad@users.noreply.github.com>
2023-04-04 13:23:53 -0500

    Attempt # 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter the length of the rectangle");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter the width of the rectangle");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+
+        Console.WriteLine("The area of your rectangle is: " + area);
+    }
+}
